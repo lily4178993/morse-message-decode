be3c3d8846d2d9769b6aba56c53ca5cfda5f4139
lily4178993 <nellytelli@gmail.com>
2023-11-10 13:10:54 +0000

    feat: add some morse code to decode

--- morse_code_message.rb
+++ morse_code_message.rb
@@ -8,17 +8,20 @@ MORSE_CODE = {
   ' ' => ' '
 }.freeze
 
-
 def decode_char(morse_code)
   MORSE_CODE[morse_code] || '?'
 end
 
 def decode_word(morse_word)
   morse_code_to_words = morse_word.split
-  word = morse_code_to_words.map { |code| decode_char(code) }.join
+  morse_code_to_words.map { |code| decode_char(code) }.join
 end
 
 def decode_sentence(morse_sentence)
   morse_code_to_sentence = morse_sentence.split('   ')
-  sentence = morse_code_to_sentence.map { |word| decode_word(word) }.join(' ')
+  morse_code_to_sentence.map { |word| decode_word(word) }.join(' ')
 end
+
+puts decode_sentence('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+
+puts decode_sentence('-- -.--   -. .- -- .')
