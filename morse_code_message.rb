11f13e9b979b3e84777b88a7d0e90c77c39f0eae
rmiyoyo <miyoyoraphael@gmail.com>
2023-11-10 16:05:13 +0300

    Implement deoode message

--- morse_code_message.rb
+++ morse_code_message.rb
@@ -7,3 +7,18 @@ MORSE_CODE = {
   '--..' => 'Z',
   ' ' => ' '
 }.freeze
+
+
+def decode_char(morse_code)
+  MORSE_CODE[morse_code] || '?'
+end
+
+def decode_word(morse_word)
+  morse_code_to_words = morse_word.split
+  word = morse_code_to_words.map { |code| decode_char(code) }.join
+end
+
+def decode_sentence(morse_sentence)
+  morse_code_to_sentence = morse_sentence.split('   ')
+  sentence = morse_code_to_sentence.map { |word| decode_word(word) }.join(' ')
+end
